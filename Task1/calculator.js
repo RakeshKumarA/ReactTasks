d14e1d97b06f622d11aba0031df7b99197e4c5bb
RakeshKumarA <rkakumar85@gmail.com>
2019-10-15 20:22:18 +0200

    Added Calculator Functions

---
+++ Task1/calculator.js
@@ -0,0 +1,14 @@
+const calculate = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    case '/':
+      return num1 / num2;
+  }
+};
+
+console.log(calculate(1, 2, '+'));
